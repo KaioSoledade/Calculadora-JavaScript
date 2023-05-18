1d3faf23142bcd64cd92894104cc6b40483855ab
Kaio Soledade <soledadekaio@gmail.com>
2023-05-17 22:51:34 -0300

    Calculadora em HTML e CSS quase finalizada

---
+++ script.js
@@ -0,0 +1,12 @@
+
+        
+
+window.confirm("Você esta preparado?")
+var n1 = Number(window.prompt("Digite um numero"))
+var n2 = Number(window.prompt("Digite outro numero"))
+
+var soma = n1 + n2
+var mult = n1 * n2
+var div = n1 / n2
+window.alert (`A soma é ${soma}. A multiplicação é ${mult},e a Divisão é ${div}`)
+
