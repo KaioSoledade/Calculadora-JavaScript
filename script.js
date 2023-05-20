da27d9699cc2b496e50ef69bad3e7afa98e0bc44
Kaio Soledade <soledadekaio@gmail.com>
2023-05-19 23:17:22 -0300

    segunda calculadora

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ var mult = n1 * n2
 var div = n1 / n2
 window.alert (`A soma é ${soma}. A multiplicação é ${mult},e a Divisão é ${div}`)
 
+var result1 = window.document.getElementsByTagName('h1')[0]
+
+window.document.write(result1.innerText)
+
+var fundo = window.document.body
+
+fundo.style.background = 'black'
